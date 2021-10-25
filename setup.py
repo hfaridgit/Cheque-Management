5d1df7587d7948abf575302881898a9d7922497e
hfaridgit <h.farid@egdirection.com>
2021-10-25 13:02:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
-from pip.req import parse_requirements
-import re, ast
+from __future__ import unicode_literals
 
+import ast
+import re
+
+from setuptools import find_packages, setup
 # get version from __version__ variable in cheque_management/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+	
 with open('cheque_management/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='cheque_management',
@@ -21,6 +24,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
